b1cd24daa3f495cff69a42c284b1399d09b98a49
tantaman <matt.crinklaw@gmail.com>
2020-02-02 19:29:58 -0500

    refactor add-remove to not require a flag

--- src/view/AddRemoveCardBtn.react.js
+++ src/view/AddRemoveCardBtn.react.js
@@ -3,13 +3,12 @@ import "./AddRemoveCardBtn.css";
 import Deck from "../model/Deck";
 
 type Props = $ReadOnly<{|
-  mode: "add" | "remove",
-  deck: Deck
+  onRemove
 |}>;
 
-function AddRemoveCardBtn({ mode }: Props) {
-  const onRemove = () => null;
-  const onAdd = () => null;
+function AddRemoveCardBtn({ onRemove, onAdd }: Props) {
+  invariant(onRemove == null || onAdd == null, 'You cannot specify both on add and onRemove');
+  invariant(!(onRemove == null && onAdd == null), 'You must specify either onAdd on onRemove')
 
   let classNames = "AddRemoveCardBtn";
   let symbol = "+";
