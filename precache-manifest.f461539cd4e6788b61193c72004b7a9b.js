aee9bc19a9ba342870cdca507b6611e0e5b279e5
tantaman <matt.crinklaw@gmail.com>
2020-02-03 10:53:06 -0500

    Updates

---
+++ precache-manifest.f461539cd4e6788b61193c72004b7a9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d23ee34142a5420c1815757fadf2e8cd",
+    "url": "/flashcards/index.html"
+  },
+  {
+    "revision": "a2730d65127aa4d15a40",
+    "url": "/flashcards/static/css/main.daae1185.chunk.css"
+  },
+  {
+    "revision": "c8318012293d13e8b400",
+    "url": "/flashcards/static/js/2.9a51b625.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/flashcards/static/js/2.9a51b625.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2730d65127aa4d15a40",
+    "url": "/flashcards/static/js/main.2eebe18a.chunk.js"
+  },
+  {
+    "revision": "7e14da0d7f813acbe212",
+    "url": "/flashcards/static/js/runtime-main.b7770e97.js"
+  }
+]);
