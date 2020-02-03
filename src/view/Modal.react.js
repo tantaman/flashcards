9c30de2199f749d787ad60f76e0237d5fabd69d6
tantaman <matt.crinklaw@gmail.com>
2020-02-03 09:16:52 -0500

    some modal positioning

--- src/view/Modal.react.js
+++ src/view/Modal.react.js
@@ -1,9 +1,15 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import ReactDOM from "react-dom";
+import "./Modal.css";
 
 type Props = $ReadOnly<{| children: React.Node |}>;
 
 export default function Modal({ children }: Props) {
   const modalRef = useRef(document.getElementById("modal-holder"));
-  return ReactDOM.createPortal(children, modalRef.current);
+  return ReactDOM.createPortal(
+    <div className="Modal">
+      <div className="Modal-inner">{children}</div>
+    </div>,
+    modalRef.current
+  );
 }
